b107806c36d28445235c8372cd22dd8a252bc73f
PokerKid <kkaito1412.kk@gmail.com>
2014-06-07 00:21:13 +0800

    /away and /away will collide, same as /back and /back

--- commands.js
+++ commands.js
@@ -1751,14 +1751,11 @@ var commands = exports.commands = {
 		});
 	},
 
-	away: 'blockchallenges',
-	idle: 'blockchallenges',
 	blockchallenges: function (target, room, user) {
 		user.blockChallenges = true;
 		this.sendReply("You are now blocking all incoming challenge requests.");
 	},
 
-	back: 'allowchallenges',
 	allowchallenges: function (target, room, user) {
 		user.blockChallenges = false;
 		this.sendReply("You are available for challenges from now on.");
